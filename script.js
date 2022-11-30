d3450abef3c0e519914af76123df62f8af2714b3
Rost <carinikrost@gmail.com>
2022-11-30 15:30:54 +0200

    third commit

--- script.js
+++ script.js
@@ -1,65 +1,71 @@
 "use strict";
-// let numbersOfFilms;
-// function start() {
-//   numbersOfFilms = +prompt("Сколько фильвом вы посмотрели?", "");
-//   while (
-//     numbersOfFilms == "" ||
-//     numbersOfFilms == null ||
-//     isNaN(numbersOfFilms)
-//   ) {
-//     numbersOfFilms = +prompt("Сколько фильвом вы посмотрели?", "");
-//   }
-// }
-// start();
 // const personalMovieDB = {
-//   count: numbersOfFilms,
+//   count: 0,
 //   movies: {},
 //   actors: {},
 //   gender: [],
 //   privat: false,
-// };
-
-// function rememberMyFilms() {
-//   for (let i = 0; i < 2; i++) {
-//     const a = prompt("Один из последних просмотренных фильмов?", "").trim(),
-//       b = prompt("На сколько оцените его?", "");
-//     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-//       personalMovieDB.movies[a] = b;
-//       console.log("done");
-//     } else {
-//       console.log("error");
-//       i--;
+//   start: function () {
+//     personalMovieDB.count = +prompt("Сколько фильвом вы посмотрели?", "");
+//     while (
+//       personalMovieDB.count == "" ||
+//       personalMovieDB.count == null ||
+//       isNaN(personalMovieDB.count)
+//     ) {
+//       personalMovieDB.count = +prompt("Сколько фильвом вы посмотрели?", "");
 //     }
-//   }
-// }
-// rememberMyFilms();
-
-// function detectPersonalLevel() {
-//   if (personalMovieDB.count < 10) {
-//     console.log("просмотрено мало фильмов");
-//   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//     console.log("Вы классический зритель");
-//   } else if (personalMovieDB.count >= 30) {
-//     console.log("Вы киноман");
-//   } else {
-//     console.log("Произошла ошибка");
-//   }
-// }
-// detectPersonalLevel();
-
-// function showMyDB(hidden) {
-//   if (!hidden) {
-//     console.log(personalMovieDB);
-//   }
-// }
-// showMyDB(personalMovieDB.privat);
-
-// function writeYourGenres() {
-//   for (let i = 1; i <= 3; i++) {
-//     personalMovieDB.gender[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-//   }
-// }
-// writeYourGenres();
+//   },
+//   rememberMyFilms: function () {
+//     for (let i = 0; i < 2; i++) {
+//       const a = prompt("Один из последних просмотренных фильмов?", "").trim(),
+//         b = prompt("На сколько оцените его?", "");
+//       if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//       } else {
+//         console.log("error");
+//         i--;
+//       }
+//     }
+//   },
+//   detectPersonalLevel: function () {
+//     if (personalMovieDB.count < 10) {
+//       console.log("просмотрено мало фильмов");
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//       console.log("Вы классический зритель");
+//     } else if (personalMovieDB.count >= 30) {
+//       console.log("Вы киноман");
+//     } else {
+//       console.log("Произошла ошибка");
+//     }
+//   },
+//   showMyDB: function (hidden) {
+//     if (!hidden) {
+//       console.log(personalMovieDB);
+//     }
+//   },
+//   toggleVisibleMyDB: function () {
+//     if (personalMovieDB.privat) {
+//       personalMovieDB.privat = false;
+//     } else {
+//       personalMovieDB.privat = true;
+//     }
+//   },
+//   writeYourGenres: function () {
+//     for (let i = 1; i <= 3; i++) {
+//       let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+//       if (genre === "" || genre == null) {
+//         console.log("Вы ввели некорректные данные или не ввели их вовсе");
+//         i--;
+//       } else {
+//         personalMovieDB.gender[i - 1] = genre;
+//       }
+//     }
+//     personalMovieDB.gender.forEach((item, i) => {
+//       console.log(`Любимый жанр ${i + 1} - это ${item}`);
+//     });
+//   },
+// };
 
 // const options = {
 //   name: "test",
@@ -93,11 +99,61 @@
 // console.log(Object.keys(options).length)
 
 // const arr =[1,2,3,4,5,6];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a,b) {
+//   return a-b
+// }
+
 // arr.forEach(function(item,i,arr){
 //   console.log(`${i}: ${item} внутри массива ${arr}`);
 // });
 
-const str = prompt("", "");
-const products = str.split(", ");
-products.sort();
-console.log(products.join(";"));
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join(";"));
+
+// function copy(mainObj) {
+//   let objCopy = {}; // objCopy будет хранить копию mainObj
+//   let key;
+
+//   for (key in mainObj) {
+//     objCopy[key] = mainObj[key]; // копирует каждое свойство objCopy
+//   }
+//   return objCopy;
+// }
+
+const mainObj = {
+  a: 2,
+  b: 5,
+  c: {
+    x: 7,
+    y: 4,
+  },
+};
+const newNumbers = copy(mainObj);
+newNumbers.a = 10;
+console.log(newNumbers);
+console.log(mainObj);
+
+const add = {
+  d: 17,
+  e: 20,
+};
+console.log(Object.assign(mainObj, add)); //1-й аргумент в который хотим поместить, 2-й тот объект, который помещаем
+
+// const soldier = {
+//   health: 400,
+//   armor: 100,
+//   sayHello: function () {
+//     console.log("hello");
+//   },
+// };
+// const john = Object.create(soldier);
+// john.sayHello();
+
+// //Object.setPrototypeOf(john,soldier); тоже что и ниже, только правильно
+// //john.__proto__ = soldier; //устаревшее
+// console.log(john.armor);
